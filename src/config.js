a933498793b550908e0c24f47029a4f8b3133b90
Sharada Shehan <sharadashehan6@gmail.com>
2023-11-30 16:09:40 +0530

    env

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
-export const {
-  APP_PORT = 4000,
-  NODE_ENV = 'development',
+import dotenv from 'dotenv'
 
-  DB_USERNAME = 'admin',
-  DB_PASSWORD = '12345678',
-  DB_HOST = 'localhost',
-  DB_PORT = 27017,
-  DB_NAME = 'chat'
+dotenv.config()
+
+export const {
+  APP_PORT,
+  NODE_ENV,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME
 } = process.env
 
 export const IN_PROD = NODE_ENV === 'production'
